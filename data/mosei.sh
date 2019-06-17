2fe2a972472cbc4098aac19d79f5f9dc46f24712
Shreeyash Geda <sgeda@ee.iitr.ac.in>
2019-06-17 16:41:17 +0800

    Update mosei.sh

--- data/mosei.sh
+++ data/mosei.sh
@@ -1,12 +1,12 @@
 mkdir mosei
 cd mosei
 
-wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/audio_test.h5"
-wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/audio_train.h5"
-wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/audio_valid.h5"
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/audio_test.h5"
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/audio_train.h5"
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/audio_valid.h5"
 
-wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/ey_test.h5"
-wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/ey_train.h5"
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/ey_test.h5"
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/ey_train.h5"
 wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/ey_valid.h5"
 
 wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/text_test_emb.h5"
