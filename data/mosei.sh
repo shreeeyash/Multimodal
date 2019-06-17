dafaab88debaa4cc86521d93dc6520d1dc9ca224
Shreeyash Geda <sgeda@ee.iitr.ac.in>
2019-06-17 16:22:34 +0800

    Add files via upload

---
+++ data/mosei.sh
@@ -0,0 +1,22 @@
+mkdir mosei
+cd mosei
+
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/audio_test.h5"
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/audio_train.h5"
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/audio_valid.h5"
+
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/ey_test.h5"
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/ey_train.h5"
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/ey_valid.h5"
+
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/text_test_emb.h5"
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/text_train_emb.h5"
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/text_valid_emb.h5"
+
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/video_test.h5"
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/video_train.h5"
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/video_valid.h5"
+
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/y_test.h5"
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/y_train.h5"
+wget -P ../ "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosei/data/y_valid.h5"
