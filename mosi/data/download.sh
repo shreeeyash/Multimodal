963757854dafbe3bbcc582e9f8b69eb02315c2e3
Shreeyash Geda <sgeda@ee.iitr.ac.in>
2019-07-22 12:03:46 +0800

    Add files via upload

---
+++ mosi/data/download.sh
@@ -0,0 +1,9 @@
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosi/X_test.h5"
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosi/X_train.h5"
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosi/X_valid.h5"
+
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosi/y_test.h5"
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosi/y_train.h5"
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosi/y_valid.h5"
+
+wget "http://immortal.multicomp.cs.cmu.edu/raw_datasets/old_processed_data/cmu-mosi/fs_mask.pkl"
